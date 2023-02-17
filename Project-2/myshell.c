bc72385980cf1d687c95e897bfa9029d86ca8a52
Joshua Dunne <mevsthevoices@gmail.com>
2023-02-17 12:57:12 -0500

    starting to work through project 2

--- Project-2/myshell.c
+++ Project-2/myshell.c
@@ -69,6 +69,10 @@ int builtIn_jobs() {
 	
 }
 
+int builtIn_stop() {
+	
+}
+
 int builtIn_bg() {
 
 }
@@ -81,15 +85,87 @@ int builtIn_kill() {
 
 }
 
-int ifIsBuiltIn(char cmd*, char args**) {
+int getArgumentCeil(char** args) {
+	int i;	
+	for ( i = 0; args[i] != NULL; i++) { }
+	return i;
+}
+
+int isBuiltIn(char* cmd, char* arg) {
 	if (strcmp(cmd, "jobs")) {
-		return builtIn_jobs();
+		builtIn_jobs();
+		return 1;
+	}
+	else if (strcmp(cmd, "stop")) {
+		builtIn_stop();
+		return 1;
+	}
 	else if (strcmp(cmd, "bg")) {
-		return builtIn_bg();
+		builtIn_bg();
+		return 1;
+	}
 	else if (strcmp(cmd, "fg")) {
-		return builtIn_fg();
+		builtIn_fg();
+		return 1;
+	}
 	else if (strcmp(cmd, "kill")) {
-		return builtIn_kill();
+		builtIn_kill();
+		return 1;
+	}
+	return 0;
+}
+
+int doForegroundCommand(char* cmd, char** args) {
+
+}
+
+int doBackgroundCommand(char* cmd, char** args) {
+
+}
+
+int isCommand(char* cmd, char** args) {
+	int i;
+	int numberOfArguments;
+	int lengthOfFinalArgument;
+
+	numberOfArguments = getArgumentCeil(args);
+	for ( i = 0; (i > 0) && (args[numberOfArguments - 1][i] != '\0'); i++) { }
+	lengthOfFinalArgument = i;
+
+	if (lengthOfFinalArgument > 0) {
+		if (args[numberOfArguments - 1][lengthOfFinalArgument - 1]
+			       == '&') {
+			/* here we're launching a background command 
+			 * because & occured as the last character
+			 * of the last argument*/	
+
+			args[numberOfArguments - 1][lengthOfFinalArgument - 1] = '\0';
+			return doBackgroundCommand(cmd, args);
+		}
+		int lastIndexOfCommand;
+		for (lastIndexOfCommand = 0; 
+				(cmd[lastIndexOfCommand] != '\0') &&
+				(cmd[lastIndexOfCommand + 1] != '\0');
+				 lastIndexOfCommand++) {}
+		if (cmd[lastIndexOfCommand] == '&') {
+			/* here we're launching as background
+			 * because & was the last character
+			 * of the command */
+			cmd[lastIndexOfCommand] = '&';
+			return doBackgroundCommand(cmd, args);
+		}
+	}
+}
+
+int doIntro() {
+	printf("Welcome to kbshell. My pid is %d\n", getpid());
+	return 0;
+}
+
+int doPrompt() {
+	static int count = 0;
+	static const char* initials = "jd";
+	printf("%sshell[%d]: ", initials, count++);
 	return 0;
 }
 
@@ -98,10 +174,12 @@ int main(void)
 char inputBuffer[MAX_LINE];      /* buffer to hold the command entered */
     int background;              /* equals 1 if a command is followed by '&' */
     char *args[(MAX_LINE/2)+1];  /* command line (of 80) has max of 40 arguments */
- 
+
+    doIntro(); 
     while (1){            /* Program terminates normally inside setup */
        background = 0;
-       printf(" COMMAND->\n");
+       doPrompt();
+       fflush(stdout);
        setup(inputBuffer,args,&background);       /* get next command */
 
       /* the steps are:

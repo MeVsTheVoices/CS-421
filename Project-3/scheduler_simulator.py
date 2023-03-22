c89527ca11802089431df0233a70b247ba3b9e3e
Joshua Dunne <mevsthevoices@gmail.com>
2023-03-21 18:18:18 -0700

    project 3 implements all functionality demanded, it just needs a driver

--- Project-3/scheduler_simulator.py
+++ Project-3/scheduler_simulator.py
@@ -11,6 +11,7 @@ class SchedulerStrategy:
 		pass
 
 class FirstComeFirstServe(SchedulerStrategy):
+	#simplest method, sort by arrival time
 	def do_simulation(self, processes):
 		results = []
 		readyQueue = processes
@@ -23,24 +24,25 @@ class ShortestJobFirst(SchedulerStrategy):
 	def do_simulation(self, processes):
 		results = []
 		readyQueue = processes
-		readyQueue.sort(key= lambda x: (x[1]))
+		#sort by burst then arrival time
+		readyQueue.sort(key= lambda x: (x[1], x[2]))
 		elapsed = 0
+		#use to disreguard those that haven't arrived
 		oneWasntFinished = True
-		while oneWasntFinished:
+		while (oneWasntFinished):
 			oneWasntFinished = False
 			for i in readyQueue:
-				if (i[2] > elapsed):
-					oneWasntFinished = True
-					next
-				elif (i[1] == 0):
-					next
-				else:
-					oneWasntFinished = True
+				#the only jobs we inspect are those still with run times
+				#that are greater than zero and that have arrived at
+				#the scheduler in time to be dispatched
+				if (elapsed >= i[2]) and (i[1] > 0):
 					results.append([i[0], i[1]])
-					i[1] = 0
 					elapsed = elapsed + i[1]
+					i[1] = 0
+					oneWasntFinished = True
 					break
 		return results
+					
 		
 class RoundRobin(SchedulerStrategy):
 	#keep the quantum as a class variable
@@ -66,24 +68,25 @@ class RoundRobin(SchedulerStrategy):
 		while (oneWasntFinished):
 			oneWasntFinished = False
 			for i in readyQueue:
+				print(readyQueue)
 				#skip over all finished processes
 				if i[1] <= 0:
-					next
+					continue
+				#and all that haven't arrived yet
 				elif i[2] > elapsed:
-					next
+					continue
 				#if we'd exceed quantum
 				elif i[1] >= self.quantum:
 					oneWasntFinished = True
 					i[1] = i[1] - self.quantum
 					results.append([i[0], self.quantum])
-
 					elapsed = elapsed + self.quantum
 				#if we'd not exceed quantum
-				else:
+				elif i[1] < self.quantum:
 					oneWasntFinished = True
 					results.append([i[0], i[1]])
-					i[1] = 0
 					elapsed = elapsed + i[1]
+					i[1] = 0
 
 		return results
 		
@@ -99,37 +102,52 @@ def get_processes_from_file(filename):
 	return processes
 
 def summary_from_results(results):
+	#form an empty list
 	summary = []
+	#grab only the names of the processes
 	processNames = list({c[0] for c in results})
+	#iterate on this
 	for name in processNames:
 		waitingTime = 0
 		executionTime = 0
 		wasLast = 0
+		#use only execution times and sequence to rebuild
 		for result in results:
 			if name != result[0]:
 				waitingTime = waitingTime + result[1]
 			else:
 				executionTime = executionTime + result[1]
 				wasLast = waitingTime
+		#little arithmetic to ignore any inspected results
+		#that weren't part of the processes wait time
 		waitingTime = (wasLast - waitingTime) + waitingTime
 		summary.append([name, waitingTime, executionTime, waitingTime + executionTime])
-	return summary.sort(key = lambda x: (x[0]))
+	#give back an answer sorted by names
+	summary.sort(key = lambda x: (x[0]))
+	return summary
 
 def print_gant_chart(results):
+	#base the horizontal size roughly on the scale of the terminal
 	columns, _ = get_terminal_size()
-	columns = (columns - len(results)) * 0.75
+	#columns - 2 * len corrects for extra | characters
+	#and the names of the processes (characters)
+	columns = (columns - 2*len(results)) * 0.75
 	totalTime = 0
 	for i in results:
 		totalTime = totalTime + i[1]
 	totalSegments = len(results)
 	print('|', end="")
 	for i in results:
+		#get ourselves the length of a segment and base
+		#our outputs on that length
 		at = 0
 		limit = ceil(columns*(i[1]/totalTime))
 		half = limit // 2
 		for j in range(limit):
+			#print the name of the process in the middle of the segment
 			if at == half:
 				print(i[0], end="")
+			#print a character to space everything else
 			else :
 				print('-', end="")
 			at = at + 1
